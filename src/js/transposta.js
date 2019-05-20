40dc48f87902815eaaace7c2d2afcf25796ceb47
Jeú Chaves <jeuchaveslima@gmail.com>
2019-05-20 17:18:08 -0300

    Finalização da matriz transposta

--- src/js/transposta.js
+++ src/js/transposta.js
@@ -3,9 +3,165 @@ let utils = require("./utils");
 let OperacaoSoma = require("./operacao_soma");
 
 class OperacaoTranposta extends OperacaoSoma {
-    redimensionarMatrizes() {
-        alert("oi");
+  
+  realizarOperacao() {
+    let { numLinhas, numColunas } = this;
+
+    if (
+      !utils.matrizEstaPreenchida("a", numLinhas, numColunas)
+    ){
+        utils.mostraErroEntradaIncompleta();
+        return;
+    } else {
+        $("#erro-input-vazio").html("");
     }
+
+    $("#passo-a-passo").html(utils.criarPassoAPasso());
+
+    this.mA = utils.recuperarMatriz("a", numLinhas, numColunas);
+    this.mC = this.calcular();
+
+    this.atualizarResultado();
+  }
+
+  calcular(op) {
+    let { numLinhas, numColunas } = this;
+    let resp = Array(numColunas);
+
+    for (let l = 1; l <= numColunas; ++l) {
+      resp[l] = Array(numLinhas);
+
+      for (let c = 1; c <= numLinhas; ++c) {
+        resp[l][c] = this.mA[c][l];
+      }
+    }
+
+    return resp;
+  }
+
+  gerarEnunciado(verbo) {
+
+    let html = `<p class="lead">`;
+    let idx = `${this.linhaAtiva}${this.colunaAtiva}`;
+    html += `Vai aparecer um texto explicando aqui`;
+    html += `</p>`;
+
+    return html;
+  }
+
+  imprimirResultado() {
+    let html = "";
+
+    if (this.estagio == 4) {
+      html += "";
+    } else {
+      html += this.gerarEnunciado();
+    }
+
+    html += `<div class="col-xl-12 row">`;
+
+    html += `<div class="col-md-6">`;
+    html += this.gerarMatriz("Matriz A", this.mA, false, "a");
+    html += `</div>`;
+
+    html += `<div class="col-md-6">`;
+    html += this.gerarMatriz("Matriz Resultante", this.mC, false, "c");
+    html += `</div>`;
+
+    html += ``;
+
+    return html;
+  }
+
+  gerarMatriz(titulo, matriz, mostrarCalculo, prefixo) {
+    let html = `<h5 class="card-title text-center conteudo">${titulo}</h5>`;
+    html += `<table class="table table-bordered">`;
+
+    let numLinhas = this.numLinhas;
+    let numColunas = this.numColunas;
+
+    /**
+     * Caso for a matriz de prefixo c,
+     * o número de linhas será o número de colunas e o de colunas o de linhas
+     */
+    if (prefixo == "c") {
+      numLinhas = this.numColunas;
+      numColunas = this.numLinhas;
+    }
+    
+    for (let l = 1; l <= numLinhas; ++l) {
+      html += "<tr>";
+
+      for (let c = 1; c <= numColunas; ++c) {
+        if (
+          (l == this.linhaAtiva && c == this.colunaAtiva && prefixo == "c") ||
+          (l == this.colunaAtiva && c == this.linhaAtiva && prefixo == "a") 
+        ){
+          if (mostrarCalculo) {
+            html += `<td class="alert-dark">`;
+            html += this.gerarCalculo(l, c);
+            html += `</td>`;
+          } else {
+            html += `<td class="alert-dark">(${prefixo}<sub>${l}${c}</sub>) <strong>${
+              matriz[l][c]
+            }</strong></td>`;
+          }
+        } else {
+          html += `<td>${matriz[l][c]}</td>`;
+        }
+      }
+      html += "</tr>";
+    }
+
+    html += "</table>";
+    return html;
+  }
+
+  atualizarEstagio() {
+    let { numLinhas, numColunas, linhaAtiva, colunaAtiva } = this;
+
+    if (linhaAtiva == 1 && colunaAtiva == 1) {
+      this.estagio = 1;
+      return;
+    }
+
+    if (linhaAtiva == numColunas && colunaAtiva == numLinhas) {
+      this.estagio = 3;
+      return;
+    }
+
+    if (linhaAtiva == 0 && colunaAtiva == 0) {
+      this.estagio = 4;
+    } else {
+      this.estagio = 2;
+    }
+  }
+
+  voltar() {
+    if (this.colunaAtiva == 1) {
+      if (this.linhaAtiva > 1) {
+        --this.linhaAtiva;
+        this.colunaAtiva = this.numLinhas;
+      }
+    } else {
+      --this.colunaAtiva;
+    }
+
+    this.atualizarResultado();
+  }
+
+  avancar() {
+    if (this.colunaAtiva == this.numLinhas) {
+      if (this.linhaAtiva < this.numColunas) {
+        ++this.linhaAtiva;
+        this.colunaAtiva = 1;
+      }
+    } else {
+      ++this.colunaAtiva;
+    }
+
+    this.atualizarResultado();
+  }
 }
 
 $(function() {
